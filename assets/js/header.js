7069690a93424711d2d560dd8475886693ad35f1
FabricP <pfabricio161@gmail.com>
2026-08-31 13:07:36 -0300

    finalização do footer

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,5 @@
-async function loadComponents(element, file){
-    window.document.getElementById(element).innerHTML = await (await fetch(file)).text();
+async function loadComponents(element, file){                                               /* Função assincrona pois preciso que o código espere o "fetch" encontrar o arquivo "header.html" */
+    window.document.getElementById(element).innerHTML = await (await fetch(file)).text();   /* "await" faz o código esperar até que o "fetch()" e o ".text" retornem com o resultado */
 }
 
 loadComponents('header-placeholder', '/components/header.html');
