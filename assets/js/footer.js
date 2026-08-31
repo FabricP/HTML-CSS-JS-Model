2dc5a05bb0231ac2bd0ffcd16855fc551e9a1be4
FabricP <pfabricio161@gmail.com>
2026-08-31 17:29:32 -0300

    inicio do sistema de menu flutuante

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,5 +1,5 @@
 async function loadComponents(element, file){                                               /* Função assincrona pois preciso que o código espere o "fetch" encontrar o arquivo "footer.html" */
-    window.document.getElementById(element).innerHTML = await (await fetch(file)).text();   /* "await" faz o código esperar até que o "fetch()" e o ".text" retornem com o resultado */
+    window.document.getElementById(element).innerHTML = await (await fetch(file)).text()    /* "await" faz o código esperar até que o "fetch()" e o ".text" retornem com o resultado */
 }
 
-loadComponents('footer-placeholder', '/components/footer.html');
+loadComponents('footer-placeholder', '/components/footer.html')
--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,10 @@
 async function loadComponents(element, file){                                               /* Função assincrona pois preciso que o código espere o "fetch" encontrar o arquivo "header.html" */
-    window.document.getElementById(element).innerHTML = await (await fetch(file)).text();   /* "await" faz o código esperar até que o "fetch()" e o ".text" retornem com o resultado */
+    window.document.getElementById(element).innerHTML = await (await fetch(file)).text()    /* "await" faz o código esperar até que o "fetch()" e o ".text" retornem com o resultado */
 }
 
-loadComponents('header-placeholder', '/components/header.html');
+loadComponents('header-placeholder', '/components/header.html')
+
+/* Teste de menu flutuante */
+function showmenu(element, status){
+    window.document.getElementById(element).style = status
+}
