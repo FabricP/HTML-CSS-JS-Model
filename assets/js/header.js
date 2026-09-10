07e4ceeb234fdea069f1f095917659ad001fc356
Fabricio Pereira <pfabricio161@gmail.com>
2026-09-09 22:54:11 -0300

    finalização da lógica dos submenus do header

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,21 +1,19 @@
-async function loadComponents(element, file){                                               /* Função assincrona pois preciso que o código espere o "fetch" encontrar o arquivo "header.html" */
-    window.document.getElementById(element).innerHTML = await (await fetch(file)).text()    /* "await" faz o código esperar até que o "fetch()" e o ".text" retornem com o resultado */
+async function loadComponents(id, file){                                               /* Função assincrona pois preciso que o código espere o "fetch" encontrar o arquivo "header.html" */
+    window.document.getElementById(id).innerHTML = await (await fetch(file)).text()    /* "await" faz o código esperar até que o "fetch()" e o ".text" retornem com o resultado */
 }
 
-loadComponents('header-placeholder', '/components/header.html')
-
-/* Teste de menu flutuante */
-let visible
-function showmenu(element){
-    if (visible == true){
-        window.document.getElementById(element).style = 'display: none;'
-        visible = false
+function showMenu(id){
+    let element_id = window.document.getElementById(id)                     /* Encontra o elemento que foi clicado */
+    
+    if (element_id.style.display == 'flex'){                                /* Comparação para saber se o menu está aberto */
+        element_id.style.display =  'none'                                  /* Caso esteja aberto o menu é fechado */
     } else{
-        window.document.getElementById(element).style = 'display: flex;'
-        visible = true
+        for (let position of document.getElementsByClassName('submenu')){   /* Para cada posição do array da classe submenu... */
+            position.style.display = 'none'                                 /* Fecha todos os menus abertos */
+        }
+
+        element_id.style.display = 'flex'                                   /* Abre o menu que foi clicado */
     }
 }
 
-function mouseover(element, status){
-    window.document.getElementById(element).style = status
-}
+loadComponents('header-placeholder', '/components/header.html')
